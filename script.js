bbd8a5df0fefa80fdcf4f9d7df9b9c55310631d8
Barbyie <thiago.barbieri2002@gmail.com>
2024-02-27 08:48:16 -0300

    Create operate function

--- script.js
+++ script.js
@@ -14,3 +14,11 @@ const divide = function(num1, num2) {
   return (num1 / num2);
 };
 
+let firstNumber = 0;
+let secondNumber = 0;
+let operator = "";
+
+const operate = function(num1, operator, num2) {
+  return operator(num1, num2);
+};
+
